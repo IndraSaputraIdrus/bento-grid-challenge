a67475067fd40580021ef728561f23685aa7c65d
indra saputra idrus <indrasaputraidrus@gmail.com>
2025-01-28 15:28:16 +0800

    component 2

---
+++ src/lib/components/comp-4.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	const chartData = [
+		{
+			hour: 12,
+			period: 'am'
+		},
+		{
+			hour: 3,
+			period: 'am'
+		},
+		{
+			hour: 6,
+			period: 'am'
+		},
+		{
+			hour: 9,
+			period: 'am'
+		},
+		{
+			hour: 12,
+			period: 'pm'
+		},
+		{
+			hour: 3,
+			period: 'pm'
+		},
+		{
+			hour: 6,
+			period: 'pm'
+		},
+		{
+			hour: 9,
+			period: 'pm'
+		}
+	];
+</script>
+
+<div>
+	<div>
+		<h2>Schedule to social media.</h2>
+	</div>
+	<div>
+		<p>Best Time to Post</p>
+		<hr />
+		<div>
+			<ul>
+				<li>Mon</li>
+				<li>Tue</li>
+				<li>Wed</li>
+				<li>Thu</li>
+				<li>Sat</li>
+				<li>Sun</li>
+			</ul>
+
+			<ul>
+				{#each chartData as data}
+					<li>
+						<div></div>
+						<span>{data.hour}{data.period.charAt(0)}</span>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+	<div>
+		<p>Optimize post timings to publish content at the perfect time for your audience.</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
 	import { MediaWidth, Comp1, Comp2 } from '$lib/components';
 </script>
 
-<div class="min-h-dvh text-neutral-black text-lg font-dm-sans">
-	<main class="px-4 py-6">
-		<div class="grid grid-cols-1">
+<div class="min-h-dvh bg-accent-purple-100 text-neutral-black text-lg font-dm-sans">
+	<main class="px-4 py-8 mb-50">
+		<div class="grid grid-cols-1 gap-8">
 			<Comp1 />
+      <Comp2 />
 			<MediaWidth />
 		</div>
 	</main>
