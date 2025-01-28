3741ecca4b6c05f57712406ee64887e1630809c8
indra saputra idrus <indrasaputraidrus@gmail.com>
2025-01-29 00:36:43 +0800

    component 4: add transition

--- src/lib/components/comp-4.svelte
+++ src/lib/components/comp-4.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { scale } from 'svelte/transition';
 	import { chartData } from './data';
 	let selected = $state('Mon');
 	const activeChartData = $derived(chartData[selected]);
@@ -8,29 +9,34 @@
 </script>
 
 {#snippet chartElement()}
-	<ul class="text-neutral-black/50 flex h-39 items-end justify-between text-xs">
-		{#each activeChartData as data, index}
-			{@const mossActive = data.count + index * 0.1 === maxCount}
-			<li class="relative flex flex-col items-center gap-3">
-				{#if mossActive}
-					<span
-						class={[
-							'absolute z-[999] w-max rounded-lg',
-							'bg-green-200 text-green-800',
-							'px-2 py-1',
-							'font-medium shadow',
-							'-top-10'
-						]}>Most active</span
-					>
-				{/if}
-				<div
-					style="height: {data.count * 8}px"
-					class={['bg-accent-purple-500 w-4 rounded-full']}
-				></div>
-				<span>{data.hour}{data.period.charAt(0)}</span>
-			</li>
-		{/each}
-	</ul>
+	{#key activeChartData}
+		<ul
+			in:scale={{ duration: 200 }}
+			class="text-neutral-black/50 flex h-39 items-end justify-between text-xs"
+		>
+			{#each activeChartData as data, index}
+				{@const mossActive = data.count + index * 0.1 === maxCount}
+				<li class="relative flex flex-col items-center gap-3">
+					{#if mossActive}
+						<span
+							class={[
+								'absolute z-[999] w-max rounded-lg',
+								'bg-green-200 text-green-800',
+								'px-2 py-1',
+								'font-medium shadow',
+								'-top-10'
+							]}>Most active</span
+						>
+					{/if}
+					<div
+						style="height: {data.count * 8}px"
+						class={['bg-accent-purple-500 w-4 rounded-full']}
+					></div>
+					<span>{data.hour}{data.period.charAt(0)}</span>
+				</li>
+			{/each}
+		</ul>
+	{/key}
 {/snippet}
 
 <div class="bg-accent-purple-100 max-w-88 space-y-4 rounded-lg p-4 md:w-min">
